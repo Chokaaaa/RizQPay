6392b25bf5241410da272de3bd6f9ef29cb598dd
Nursultan <nursultanyelemessov@gmail.com>
2025-08-24 22:44:40 +0400

    Initial Commit

---
+++ RizQPay/RizQPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  RizQPayApp.swift
+//  RizQPay
+//
+//  Created by Nursultan Yelemessov on 24/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct RizQPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
